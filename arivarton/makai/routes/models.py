f351c2a7907b9db6563cb0bd34695abc29b79b14
arivarton <accounts@arivarton.com>
2019-02-03 21:20:41 +0000

    Added view restriction to deleted collection.

--- arivarton/makai/routes/models.py
+++ arivarton/makai/routes/models.py
@@ -49,32 +49,44 @@ class RoutePage(Page):
     def get_parent_collection_name(self):
         return self.__class__.__name__
 
+    def get_root_collection(self):
+        return Collection.objects.get(name='Root')
+
     def get_parent_collection(self):
         ''' Set up inital stages for a image collection. '''
-        parent_name = get_parent_collection_name()
+        parent_name = self.get_parent_collection_name()
         try:
             parent_collection = Collection.objects.get(name=parent_name)
         except Collection.DoesNotExist:
-            root_collection = Collection.objects.get(name='Root')
+            root_collection = self.get_root_collection()
             parent_collection = root_collection.add_child(name=parent_name)
 
         return parent_collection
 
     def get_deleted_collection(self):
-        deleted_collection_name = '_deleted from %s' % self.get_parent_collection_name()
-        # Setup collection to store deleted collections
+        deleted_collection_name = '_deleted'
+        collection_group_name = 'Deleted collections'
+        parent_collection = self.get_parent_collection()
         try:
-            deleted_collection = Collection.objects.get(name=deleted_collection_name)
+            deleted_collection = parent_collection.get_children().get(name=deleted_collection_name)
         except Collection.DoesNotExist:
-            parent_collection = self.get_parent_collection()
             deleted_collection = parent_collection.add_child(name=deleted_collection_name)
+            try:
+                collection_group = Group.objects.get(name=collection_group_name)
+            except Group.DoesNotExist:
+                collection_group = Group(name=collection_group_name)
+                collection_group.save()
+            collection_restriction = CollectionViewRestriction(restriction_type='groups',
+                                                               collection=deleted_collection)
+            collection_restriction.save()
+            collection_restriction.groups.set((collection_group,))
 
         return deleted_collection
 
     def set_collection(self):
         ''' Set the collection when creating or editing a RoutePage.
         Every RoutePage needs a separate collection for images.'''
-        parent_name = get_parent_collection_name()
+        parent_name = self.get_parent_collection_name()
         collection_name = '%s - %s' % (parent_name, self.slug)
 
         # If RoutePage has not been previously created, the parent for all RoutePage collections
@@ -92,3 +104,10 @@ class RoutePage(Page):
             )
             self.image_collection = page_collection
             self.save()
+
+    def clean_collection(self):
+        ''' When deleting page move connected collection to a waste collection. '''
+        deleted_collection = self.get_deleted_collection()
+        # Setup collection to store deleted collections
+        if self.image_collection.get_parent() != deleted_collection:
+            self.image_collection.move(deleted_collection, pos='last-child')
